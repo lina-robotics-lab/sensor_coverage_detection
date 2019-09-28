9bb38333849cc4bae2197c89550b135c9fe6e79d
victor-qin <lingheng.qin@gmail.com>
2019-09-27 22:58:59 -0400

    First Commit

---
+++ Map.m
@@ -0,0 +1,15 @@
+limit = 10000;
+
+time = 0;
+dt = 0.01;
+target = TargetClass([0,0]);
+target.incrementTime(dt);
+
+for i = 1:limit
+    target.motionUpdate();
+    target.incrementTime(dt);
+end
+
+% target.states
+
+plot(target.states(1:end, 1), target.states(1:end, 2));
