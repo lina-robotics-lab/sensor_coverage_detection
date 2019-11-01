19da0a2a8d3e6b16552939efd685e27be09ba371
victor-qin <lingheng.qin@gmail.com>
2019-11-01 12:06:43 -0400

    Working on quaternion and euler angular differences

--- Static_Target.m
+++ Static_Target.m
@@ -1,24 +1,22 @@
 close all;
-clear all;
 
 s1 = SensorClass([0, 1], [0,0]);
 s1.measureTarget([0,0]);
-% s2 = SensorClass([0.05, 0.9987], [0,0]);
-s2 = SensorClass([1, 0], [0,0]);
+s2 = SensorClass([0.05, 0.9987], [0,0]);
+% s2 = SensorClass([1, 0], [0,0]);
 s2.measureTarget([0,0]);
-% s3 = SensorClass([-0.05, 0.9987], [0,0]);
-s3 = SensorClass([-1, 0], [0,0]);
+s3 = SensorClass([-0.05, 0.9987], [0,0]);
+% s3 = SensorClass([-1, 0], [0,0]);
 s3.measureTarget([0,0]);
 
 sensors = [s1, s2, s3];
 
 for i = 1:50
-      
+   angles = [s3.measureTarget([0,0]), s1.measureTarget([0,0]), s2.measureTarget([0,0]), s3.measureTarget([0,0]), s1.measureTarget([0,0])];
    for j = 1:length(sensors)
-      angles = [s3.measureTarget([0,0]), s1.measureTarget([0,0]), s2.measureTarget([0,0]), s3.measureTarget([0,0]), s1.measureTarget([0,0])];
-      sensors(j).moveSensor(angles(j), angles(j+2)); 
+      sensors(j).moveSensor(angles(j+2), angles(j)); 
    end
-   
+%    disp("derp");
 end
 
 out = [s1.returnAngle(), s2.returnAngle(), s3.returnAngle()]
