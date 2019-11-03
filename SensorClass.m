73bca6c1ffc61ff10ad50295f7ad16ca45e48556
Peter Tianpeng ZHANG <tzhangam@gmail.com>
2019-11-03 15:52:47 -0500

    Finished: cartesian coordinate to polar coordinate conversion. Equi-angular sensor placement control. To see the result, run Static_Target.m; Next: implement moving target functionality

--- SensorClass.m
+++ SensorClass.m
@@ -2,6 +2,9 @@ classdef SensorClass < handle
    
     properties
         time = 0;
+        % Implementation conventions: 
+        % 1. Angles should always vary between 0 and 2*pi
+        % 2. Angle value increases in counter-clockwise(ccw) direction.
         angle = 0;
         states = [];    % track the angle along w/ x,y
         sigma = 0.1;        
@@ -11,7 +14,8 @@ classdef SensorClass < handle
     methods
         function obj = SensorClass(location, target)
             distance = location - target;
-            obj.angle = atan2(distance(2), distance(1));%atan(location(2)/location(1));
+            [theta,rho]= cart2pol(distance(1), distance(2));
+            obj.angle=mod(theta,2*pi);%angles should always vary between 0 and 2*pi
             temp = [obj.states; [location, obj.angle, obj.time]];  
             obj.states = temp;
             obj.sigma = 0.001;
@@ -31,47 +35,28 @@ classdef SensorClass < handle
         
         function r = measureTarget(obj, target) % make angle calc to target
             location = obj.states(end, 1:2) - target;
-            obj.angle = atan2(location(2), location(1));
+            
+            %[theta,rho]=cart2pol(x,y)
+            [theta,rho]= cart2pol(location(1), location(2));%cart2pol returns an angle between -pi and pi
+            obj.angle=mod(theta,2*pi);%angles should always vary between 0 and 2*pi
             r = obj.angle;
         end
         
-        % Finds angle between two sensors
-        function r = angleFinder(obj, neighbor, own)
-            ownQuat = eul2quat([0, own, 0]);
-            ownQuat = ownQuat / norm(ownQuat);
-            neighQuat = eul2quat([0, neighbor, 0]);
-            neighQuat = neighQuat / norm(neighQuat);
-            totQuat = quatmultiply(quatconj(ownQuat), neighQuat);
-            
-            dif = 2 * atan2(norm(totQuat(2:4)),totQuat(1));
-            
-%             dif = 0;
-%             if(sign(neighbor) == sign(own))
-%                 dif = own - neighbor;
-%                 if(dif > 0)
-%                    dif = 2*pi - dif; 
-%                 end
-%             else
-%                dif = own - neighbor; 
-%             end
-%             
-            if(dif < 0)
-                dif = 2*pi + dif;
-            end
-            r = dif;          
+        function r = angleFinder(obj, neighbor_angle, own_angle)
+            % Both neighbor_angle and own_angle are between 0 and 2*pi.
+            % The angle difference is always between 0 and pi.
+            diff=abs(neighbor_angle-own_angle);
+            r=min(2*pi-diff,diff);
         end
-        
+
         function r = moveSensor(obj, cwNeighbor, ccwNeighbor)   % only angles to neighbors
-%             obj.angle
-%             cwNeighbor
-%             ccwNeighbor
             cwDif = obj.angleFinder(cwNeighbor, obj.angle);
             ccwDif = obj.angleFinder(ccwNeighbor, obj.angle);
-  
+             
+            angle_move = (1/4)*(ccwDif - cwDif);% This is the rule specifie by Martinez & Bullo's paper
             
-            angle_move = (1/4)*(cwDif - ccwDif);
-                       
-            obj.angle = obj.angle + angle_move;
+            % The angle must vary between 0 and 2*pi.
+            obj.angle = mod(obj.angle + angle_move,2*pi); 
             
             radius = 1;
             location = [radius*cos(obj.angle), radius*sin(obj.angle)];
