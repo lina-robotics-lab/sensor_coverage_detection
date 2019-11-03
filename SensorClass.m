622f20c1a2d07a4fc9f7705f10367fee4bce59b3
Peter Tianpeng ZHANG <tzhangam@gmail.com>
2019-11-03 17:45:46 -0500

    Finished: modify SensorClass so that the sensors can move along a circular boundary which does not centered at the target location. Run Static_Target.m to see how sensors are distributed.

--- SensorClass.m
+++ SensorClass.m
@@ -5,7 +5,7 @@ classdef SensorClass < handle
         % Implementation conventions: 
         % 1. Angles should always vary between 0 and 2*pi
         % 2. Angle value increases in counter-clockwise(ccw) direction.
-        angle = 0;
+        angle_to_target = NaN;
         
         % A row in obj.states is [x,y,angle,time].
         states = [];    % track the angle along w/ x,y
@@ -14,18 +14,22 @@ classdef SensorClass < handle
         % it takes value 1/4.
         k = 1/4;         
         
+        boundary_origin=[0,0];
+        boundary_r = 1.5;
     end
     
     methods
-        function obj = SensorClass(location, target,varargin)
-            distance = location - target;
+        function obj = SensorClass(location, boundary_origin,boundary_r,varargin)
+            obj.boundary_origin=boundary_origin;
+            obj.boundary_r = boundary_r;
             
-            [theta,rho]= cart2pol(distance(1), distance(2));
-            obj.angle=mod(theta,2*pi);%angles should always vary between 0 and 2*pi
             
-            temp = [obj.states; [location, obj.angle, obj.time]];  
+%             [theta,rho]= cart2pol(rel_loc(1), rel_loc(2));
+%             obj.angle_to_target=mod(theta,2*pi);%angles should always vary between 0 and 2*pi
+%             
+            temp = [obj.states; [location, obj.angle_to_target, obj.time]];  
             obj.states = temp;
-            
+      
             obj.k=1/4;
             if nargin>=3
                 obj.k = varargin{1};
@@ -46,12 +50,16 @@ classdef SensorClass < handle
         end
         
         function r = measureTarget(obj, target) % make angle calc to target
-            location = obj.states(end, 1:2) - target;
+            rel_loc_target = obj.states(end, 1:2) - target;
             
-            %[theta,rho]=cart2pol(x,y)
-            [theta,rho]= cart2pol(location(1), location(2));%cart2pol returns an angle between -pi and pi
-            obj.angle=mod(theta,2*pi);%angles should always vary between 0 and 2*pi
-            r = obj.angle;
+            %Syntax:[theta,rho]=cart2pol(x,y)
+            [theta,rho]= cart2pol(rel_loc_target(1), rel_loc_target(2));%cart2pol returns an angle between -pi and pi
+            obj.angle_to_target=mod(theta,2*pi);%angles should always vary between 0 and 2*pi
+            
+            temp = [obj.states; [rel_loc_target, obj.angle_to_target, obj.time]];  
+            obj.states = temp;
+          
+            r = obj.angle_to_target;
         end
         
         function r = angleFinder(obj, neighbor_angle, own_angle)
@@ -61,9 +69,9 @@ classdef SensorClass < handle
             r=min(2*pi-diff,diff);
         end
 
-        function r = moveSensor(obj, cwNeighbor, ccwNeighbor,dist_to_target)   % only angles to neighbors
-            cwDif = obj.angleFinder(cwNeighbor, obj.angle);
-            ccwDif = obj.angleFinder(ccwNeighbor, obj.angle);
+        function r = moveSensor(obj, cwNeighbor, ccwNeighbor,target_loc)   % only angles to neighbors
+            cwDif = obj.angleFinder(cwNeighbor, obj.angle_to_target);
+            ccwDif = obj.angleFinder(ccwNeighbor, obj.angle_to_target);
              
             angle_move = obj.k*(ccwDif - cwDif);
             % This is the rule specifie by Martinez & Bullo's paper.
@@ -71,12 +79,19 @@ classdef SensorClass < handle
             % convergence locations.
             
             % The angle must vary between 0 and 2*pi.
-            obj.angle = mod(obj.angle + angle_move,2*pi); 
+            obj.angle_to_target = mod(obj.angle_to_target + angle_move,2*pi); 
             
-            radius = dist_to_target;
-            location = [radius*cos(obj.angle), radius*sin(obj.angle)];
+            % Caculate the extended update location as if the sensor can
+            % move out of the boundary.
+            ext_r = dist(obj.states(end,1:2),target_loc);
+            extended_update = target_loc + ext_r*[cos(obj.angle_to_target), sin(obj.angle_to_target)];
             
-            temp = [obj.states; [location, obj.angle, obj.time]];  
+            % Project the extended location back to the boundary.
+            rel_location = extended_update - obj.boundary_origin;
+            rel_dist = dist(extended_update,obj.boundary_origin);
+            location = obj.boundary_origin+obj.boundary_r*rel_location/rel_dist; 
+            
+            temp = [obj.states; [location, obj.angle_to_target, obj.time]];  
             obj.states = temp;
             
             r = location;
@@ -84,9 +99,12 @@ classdef SensorClass < handle
         end
         
 %         function r = moveSensor(obj, move)
-%             temp = [obj.states; [location, obj.angle, obj.time]];  
+%             temp = [obj.states; [location, obj.angle_to_target, obj.time]];  
 %             obj.states = temp;
 %         end
     end
     
+end
+function r=dist(x,y)
+    r=sum((x-y).^2).^0.5;
 end
